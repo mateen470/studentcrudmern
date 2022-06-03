c3a78824bf728d90c054baab910fd8cc01ccdb18
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2022-06-03 23:50:00 +0500

    after making components

--- student/src/App.js
+++ student/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// THIS IS THE PAGE WHICH IS COMBING ALL THE COMPONENTS OF THE API
+import { Routes, Route } from "react-router-dom";
+import Axios from "axios";
+import "./App.css";
+// HERE I IMPORTED BOOTSTRAP FROM NODE MODULES BECAUSE I INSTALLED BOOTSTRAP
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// ALWAYS START THE NAME OF THE CUSTOM FILE YOU ARE IMPORTING WITH CAPITAL LETTER
+import { Head } from "./components/Head";
+import { Home } from "./components/Home";
+import { Add } from "./components/Add";
+import { Update } from "./components/Update";
+import { View } from "./components/View";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Head />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/update/:id" element={<Update />} />
+        <Route path="/view/:id" element={<View />} />
+      </Routes>
+    </>
   );
 }
 
