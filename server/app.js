c72c69a9220a042ed4b92ac7cac8b97d786f1ed1
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2022-06-04 15:52:13 +0500

    .

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const user = require("./models/schema");
 const cors = require("cors")
 const router = require("./routes/router");
+const axios =require("axios");
 
 require("./DB/connection");
 
@@ -17,14 +18,30 @@ app.use(router);
 app.use(cors({
   origin:"https://vermillion-meringue-9fb598.netlify.app"
 }));
+
+
+
+
+app.get("/", (req,res)=>{
+  res.json("SERVER STARTED");
+})
+// app.get("/", async (req, res) => {
+// 	try {axios({
+//   method: 'get',
+//   url: 'https://cors-anywhere.herokuapp.com/https://studentcrudmern.herokuapp.com',
+//   headers: {'Origin': 'https://studentcrudmern.herokuapp.com'}
+// }))
+// 		res.status(200).json(response.data);
+// 	} catch (err) {
+// 		res.status(500).json({ message: err });
+// 	}
+// });
+
 // as i used env file to store private data like port number, here i am giving path or that
 // file and then assigning it to a variable
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req,res)=>{
-  res.json("SERVER STARTED");
-})
 
 // here i am making the server listen
 app.listen(PORT, () => {
